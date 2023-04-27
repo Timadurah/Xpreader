0435d53486b8b20f67b982e89c5ba2a896d6f2ed
kilogrammie <117412844+kilogrammie@users.noreply.github.com>
2023-04-27 13:13:24 +0100

    Add files via upload

---
+++ vckxtxuvj.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * Send an SMS message by using Infobip API PHP Client.
+ *
+ * For your convenience, environment variables are already pre-populated with your account data
+ * like authentication, base URL and phone number.
+ *
+ * Please find detailed information in the readme file.
+ */
+
+require 'vendor/autoload.php';
+
+use Infobip\Api\SmsApi;
+use Infobip\Configuration;
+use Infobip\Model\SmsAdvancedTextualRequest;
+use Infobip\Model\SmsDestination;
+use Infobip\Model\SmsTextualMessage;
+
+
+
+$BASE_URL = "https://xrzp6l.api.infobip.com";
+$API_KEY = "12739989a84bb9c076bab90df68a5676-767e1b17-e8c9-4240-ac61-8aabda194515";
+$SENDERID = $_POST['from'];
+$RECEIVERs = $_POST['to'];
+$CONTENT = $_POST['info__'];
+
+
+foreach ($RECEIVERs as $RECEIVER) {
+
+
+
+
+
+$SENDER = $SENDERID;
+$RECIPIENT = $RECEIVER;
+$MESSAGE_TEXT = $CONTENT;
+
+$configuration = new Configuration(host: $BASE_URL, apiKey: $API_KEY);
+
+$sendSmsApi = new SmsApi(config: $configuration);
+
+$destination = new SmsDestination(
+    to: $RECIPIENT
+);
+
+$message = new SmsTextualMessage(destinations: [$destination], from: $SENDER, text: $MESSAGE_TEXT);
+
+$request = new SmsAdvancedTextualRequest(messages: [$message]);
+
+try {
+    $smsResponse = $sendSmsApi->sendSmsMessage($request);
+
+    echo $smsResponse->getBulkId() . PHP_EOL;
+
+    foreach ($smsResponse->getMessages() ?? [] as $message) {
+        echo sprintf('Message ID: %s, status: %s', $message->getMessageId(), $message->getStatus()?->getName()) . PHP_EOL;
+    }
+} catch (Throwable $apiException) {
+    echo("HTTP Code: " . $apiException->getCode() . "\n");
+}
+
+
+}
